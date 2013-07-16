231fb8910ecb3d5b88e3b25f5a3d888977ee1ada
Yonel Ceruto González <yceruto@abalt.org>
2013-07-16 09:13:20 -0500

    added JsonHttpResponse to serialize a dictionary to JSON format

---
+++ dajax/http/response.py
@@ -0,0 +1,22 @@
+import json
+from django.conf import settings
+from django.http import HttpResponse
+
+
+class JsonHttpResponse(HttpResponse):
+    """
+    Return a JSON serialized HTTP response
+    """
+
+    def __init__(self, data):
+        """
+        Constructor
+        """
+        serialized = json.dumps(data, sort_keys=settings.DEBUG)
+        super(JsonHttpResponse, self).__init__(
+            content=serialized,
+            content_type='application/json'
+        )
+
+    def __len__(self):
+        pass
