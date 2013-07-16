94f602d2c437dff4197738dce351faef7b1861b2
Yonel Ceruto González <yceruto@abalt.org>
2013-07-16 09:14:14 -0500

    added setup package

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-ajax-decorator',
+    version='0.1',
+    packages=['dajax'],
+    include_package_data=True,
+    license='MIT License',
+    description='Powerful and easy AJAX decorator for django views',
+    long_description=README,
+    url='',
+    author='Yonel Ceruto',
+    author_email='yceruto@abalt.org',
+    requires=['django'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
