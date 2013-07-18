e42e0a2a9ed49756a9004ab9d24544a970532847
Yonel Ceruto González <yceruto@abalt.org>
2013-07-18 09:52:05 -0500

    fixed the reference to README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
