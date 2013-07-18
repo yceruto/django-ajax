2856b7052bc4bd5383dc7ed1ab24bf4917a164d5
Yonel Ceruto González <yceruto@abalt.org>
2013-07-18 15:34:47 -0500

    removed dependences and plus performance

--- dajax/decorators/http.py
+++ dajax/decorators/http.py
@@ -6,43 +6,6 @@ from django.utils.decorators import available_attrs
 from dajax.http.response import JsonHttpResponse
 
 
-def render_to_json(mandatory=True):
-    """
-    Decorator to serialize the HTTP response to JSON. Usage::
-
-        @render_to_json()
-        def my_view(request):
-            # return a dictionary on a compulsory basis
-            return {'key': 'value', ...}
-        or
-
-        @render_to_json(False)
-        def my_view(request):
-            # use request.ajax() to see if the request was made by AJAX
-            if request.is_ajax():
-                return {'key': 'value'}
-
-            return HttpResponse('Hi! Traditional Response')
-
-    Note that when 'mandatory' is true and return a subclass of HttpResponse,
-    does not send to the browser a JSON response
-    """
-    def decorator(func):
-        @wraps(func, assigned=available_attrs(func))
-        def inner(request, *args, **kwargs):
-            data = func(request, *args, **kwargs)
-
-            if (mandatory or request.is_ajax()) and \
-                    not issubclass(type(data), HttpResponse):
-                return JsonHttpResponse(data)
-
-            return data
-
-        return inner
-
-    return decorator
-
-
 def ajax(mandatory=True):
     """
     Decorator who guesses the user response type and translates to a serialized
@@ -87,7 +50,6 @@ def ajax(mandatory=True):
     """
 
     def decorator(func):
-        @render_to_json(mandatory)
         @wraps(func, assigned=available_attrs(func))
         def inner(request, *args, **kwargs):
             if mandatory and not request.is_ajax():
@@ -100,28 +62,39 @@ def ajax(mandatory=True):
 
                     # response not allowed
                     if isinstance(data, HttpResponseNotAllowed):
-                        return dict(success=False, status=data.status_code,
-                                    method=request.method, path=request.path,
-                                    allow=data['Allow'])
+                        return JsonHttpResponse({
+                            'success': False, 'status': data.status_code,
+                            'method': request.method, 'path': request.path,
+                            'allow': data['Allow']
+                        })
 
                     # response redirect
                     if issubclass(type(data), HttpResponseRedirectBase):
-                        return dict(success=False, status=data.status_code,
-                                    location=data['Location'])
+                        return JsonHttpResponse({
+                            'success': False, 'status': data.status_code,
+                            'location': data['Location']
+                        })
 
                     # simple response
                     if issubclass(type(data), HttpResponse):
-                        return dict(success=True, status=data.status_code,
-                                    html=data.content)
+                        return JsonHttpResponse({
+                            'success': True, 'status': data.status_code,
+                            'html': data.content
+                        })
 
-                    return dict(success=True, status=200, data=data)
+                    # raw data
+                    return JsonHttpResponse({
+                        'success': True, 'status': 200, 'data': data
+                    })
 
                 except Exception as exception:
-                    return dict(success=False, status=500,
-                                exception=exception.message, path=request.path,
-                                view=func.func_name)
+                    return JsonHttpResponse({
+                        'success': False, 'status': 500,
+                        'exception': exception.message, 'path': request.path,
+                        'view': func.func_name
+                    })
             else:
-                # traditional response
+                # conventional response
                 return func(request, *args, **kwargs)
 
         return inner
