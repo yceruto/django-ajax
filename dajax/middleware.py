21b03f95b7b95e3f1b6c2fde5310b5630de41809
Yonel Ceruto González <yceruto@abalt.org>
2013-07-22 08:40:32 -0500

    added ajax middleware class

---
+++ dajax/middleware.py
@@ -0,0 +1,19 @@
+"""
+Middlewares
+"""
+from dajax.http import JsonHttpResponse
+from dajax.utils import response_to_dict
+
+
+class AjaxMiddleware(object):
+    """
+    Ajax Middleware Class
+    """
+    def process_response(self, request, response):
+        if request.is_ajax():
+            return JsonHttpResponse(response_to_dict(request, response))
+        return response
+
+    def process_exception(self, request, exception):
+        if request.is_ajax():
+            return exception
