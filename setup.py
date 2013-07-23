75b2c78fd6a425dbfed30275009d92020177862d
Yonel Ceruto González <yceruto@abalt.org>
2013-07-23 14:22:17 -0500

    fixed setup

--- setup.py
+++ setup.py
@@ -8,10 +8,11 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-ajax-decorator',
-    version='0.2',
-    packages=['dajax', 'dajax.decorators', 'dajax.http'],
+    version='0.3',
+    packages=['ajax'],
     license='MIT License',
-    description='Powerful and easy AJAX decorator and middleware for django applications',
+    description='Powerful and easy AJAX toolkit for django applications. '
+                'Contains ajax decorator, ajax middleware, shortcuts and more.',
     long_description=README,
     url='',
     author='Yonel Ceruto',
