a661198cdc147a2b6cf8f4affa00bff26aba9338
Yonel Ceruto González <yceruto@abalt.org>
2013-07-31 12:22:38 -0500

    fixed to add recursive directories and add install requires

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+from setuptools import setup, find_packages
 import os
-from sys import argv
-from setuptools import setup
 
-try:
-    if argv[1] == 'install':
-        from os.path import join
-        from distutils.sysconfig import get_python_lib
-        from distutils.dir_util import copy_tree
-        source = 'abalt_ajax'
-        destination = join(get_python_lib(), 'abalt_ajax')
-        copy_tree(source, destination)
-except IndexError:
-    pass
+CLASSIFIERS = [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+]
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -22,27 +23,22 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 version = __import__('abalt_ajax').get_version()
 
 setup(
+    author='Yonel Ceruto Glez.',
+    author_email='yceruto@abalt.org',
     name='abalt-django-ajax',
     version=version,
-    packages=['abalt_ajax'],
-    license='MIT',
     description='Powerful and easy AJAX toolkit for django applications. '
                 'Contains ajax decorator, ajax middleware, shortcuts and more.',
     long_description=README,
     url='https://github.com/yceruto/abalt-django-ajax',
-    author='Yonel Ceruto G.',
-    author_email='yceruto@abalt.org',
-    requires=['django'],
-    classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    license='MIT License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'setuptools',
+        'Django>=1.3',
     ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False
 )
