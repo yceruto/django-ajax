f9655e012d67907fb06ac1f28197ad4f7f24020b
Yonel Ceruto González <yceruto@abalt.org>
2013-10-11 07:53:15 -0500

    added the AJAXResponseMixin for Class-based views

---
+++ abalt_ajax/mixin.py
@@ -0,0 +1,19 @@
+"""
+Ajax mixin
+"""
+from abalt_ajax.decorators import ajax
+
+
+class AJAXResponseMixin(object):
+    """
+    AJAX Mixin
+    """
+    ajax_mandatory = True
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Using ajax decorator
+        """
+        return ajax(mandatory=self.ajax_mandatory)(
+            super(AJAXResponseMixin, self).dispatch
+        )(request, *args, **kwargs)
