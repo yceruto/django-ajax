141663b5f5d28763374505954244450ea66cdd4b
Yonel Ceruto González <yonelcg@sda.gae.com.cu>
2013-12-28 00:42:09 -0500

    Refactoring function ajax to Ajax class

--- abalt_ajax/static/abalt_ajax/js/jquery.ajax.js
+++ abalt_ajax/static/abalt_ajax/js/jquery.ajax.js
@@ -19,61 +19,77 @@ function csrfSafeMethod(method) {
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
 
-$.ajaxSetup({
++function ($) { "use strict";
+
+  // AJAX SETUP
+  // ======================
+
+  $.ajaxSetup({
     crossDomain: false, // obviates need for sameOrigin test
     beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type)) {
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
+      if (!csrfSafeMethod(settings.type)) {
+        xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+      }
     }
-});
+  });
 
-function _ajax(method, url, data, onsuccess, onfail, onredirect) {
-    method(url, data, function(result){
-        if (result.status == 200) {
-            if (onsuccess)
-                onsuccess(result)
-        } else {
-            var message;
+  // AJAX CLASS DEFINITION
+  // ======================
 
-            switch (result.status) {
-                case 500:
-                    message = result.exception;
-                    break;
-                case 405:
-                    message = result.method + ' ' + result.path;
-                    break;
-                case 410:
-                case 404:
-                case 403:
-                case 400:
-                case 304:
-                    message = result.path;
-                    break;
-                case 301:
-                case 302:
-                    if (onredirect)
-                        onredirect(result.status, result.path, result.location);
-                    else
-                        window.location.href = result.location;
-                    break;
-                default:
-                    message = 'An unknown error has occurred.';
-                    break;
-            }
+  var dismiss = '[data-ajax="true"]'
+  var Ajax    = function (el) {
+    $(el).on('click', dismiss, this.ajax)
+  }
 
-            if (onfail)
-                onfail(result.status, result.status_text, message);
-            else
-                alert(result.status + ' ' + result.status_text + ' - ' + message);
+  Ajax.prototype.ajax = function (e) {
+    var $this    = $(this)
+    var method = $this.attr('data-method')
+    var url = $this.attr('href')
+    var data = $this.attr('data-data')
+
+    method = method ? method.toLowerCase() : 'get'
+
+    url = url && url.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+
+    if (e) e.preventDefault()
+
+    $[method](url, data, function(result){
+      if (result.status == 200) {
+        alert(result.data)
+      } else {
+        var message = ''
+        switch (result.status) {
+          case 500:
+          case 404:
+            message = result.exception
+            break
+          case 405:
+            message = result.method
+            break
+          case 410:
+          case 403:
+          case 400:
+          case 304:
+            break
+          case 301:
+          case 302:
+            window.location.href = result.location
+            break
+          default:
+            message = 'An unknown error has occurred.'
+            break
         }
+
+        alert(result.path + '\n' + result.status + ' ' + result.status_text + '\n' + message)
+      }
     })
-}
 
-function ajax_post(url, data, onsuccess, onfail, onredirect) {
-    _ajax($.post, url, data, onsuccess, onfail, onredirect)
-}
+    if (e.isDefaultPrevented()) return
+  }
 
-function ajax_get(url, data, onsuccess, onfail, onredirect) {
-    _ajax($.get, url, data, onsuccess, onfail, onredirect)
-}
+  // ALERT DATA-API
+  // ==============
+
+  $(document).on('click.ajax.data-api', dismiss, Ajax.prototype.ajax)
+
+}(jQuery);
