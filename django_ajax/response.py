3bf60591eb1de38526044eca8121d0b197444fc1
Yonel Ceruto González <yonelcg@sda.gae.com.cu>
2014-01-29 14:29:06 -0500

    Added JSONResponse docs

--- django_ajax/response.py
+++ django_ajax/response.py
@@ -12,7 +12,15 @@ class JSONResponse(HttpResponse):
     """
     Return a JSON serialized HTTP response
     """
+
     def __init__(self, data):
+        """
+        This returns a object that we send as json content using
+        utils.serialize_to_json, that is a wrapper to json.dumps
+        method using a custom class to handle models and querysets. Put your
+        options to serialize_to_json in kwargs, other options are used by
+        response.
+        """
         super(JSONResponse, self).__init__(
             content=serialize_to_json(data, sort_keys=settings.DEBUG),
             content_type='application/json'
