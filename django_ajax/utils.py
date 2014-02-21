70da0d7902793bdecf3506332df14ee8a3b9ef6b
Yonel Ceruto González <yonelcg@sda.gae.com.cu>
2014-02-21 14:43:48 -0500

    Update traceback of exception. Use ExceptionReporter from django and convert the exception to HttpResponseServerError.

--- django_ajax/utils.py
+++ django_ajax/utils.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals
 
 import json
 
-from django.conf import settings
 from django.http.response import HttpResponseRedirectBase, HttpResponse
 from django.template.response import TemplateResponse
 from django.utils.encoding import force_text
@@ -28,10 +27,6 @@ class LazyJSONEncoder(json.JSONEncoder):
         elif issubclass(type(obj), HttpResponse):
             return obj.content
         elif issubclass(type(obj), Exception):
-            if settings.DEBUG:
-                message = traceback_exception()
-                if message:
-                    return message
             return force_text(obj)
 
         # this handles querysets and other iterable types
@@ -47,25 +42,6 @@ class LazyJSONEncoder(json.JSONEncoder):
             return force_text(obj)
 
         return super(LazyJSONEncoder, self).default(obj)
-
-
-def traceback_exception():
-    """
-    Return traceback exception
-    Includes some code from http://www.djangosnippets.org/snippets/650/
-
-    """
-    import sys
-    import traceback
-
-    exc_type, exc_info, tb = sys.exc_info()
-    if exc_type:
-        message = '%s\n%s\n\nTRACEBACK:\n' % (exc_type.__name__, exc_info)
-        for tb in traceback.format_tb(tb):
-            message += '%s\n' % tb
-        return message
-
-    return False
 
 
 def serialize_to_json(data, *args, **kwargs):
