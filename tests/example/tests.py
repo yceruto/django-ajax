881a023c41d33d81a637236417dc9bd1f9a6e16c
Yonel Ceruto <yonelceruto@gmail.com>
2017-08-27 15:00:45 -0400

    Add tests module

--- tests/example/tests.py
+++ tests/example/tests.py
@@ -1,25 +1,12 @@
-"""
-Tests
-"""
 from __future__ import unicode_literals
 from django.test import TestCase
 from django_ajax.response import JSONResponse
 
-DEBUG = True
-DEFAULT_CHARSET = 'utf-8'
-SECRET_KEY = 'j&^s-a4(uwj!fdqwr!_pa_0@#$kv8!4j(t3st44_6o^)g=ec%@'
-
-
 class ResponseTestCase(TestCase):
-    """
-    Response TestCase
-    """
     def test_json_response(self):
-        """
-        test http json response
-        """
         data = {'test': True}
         response = JSONResponse(data)
+
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/json')
         self.assertEqual(response.content, '{"test": true}')
