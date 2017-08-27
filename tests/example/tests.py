933bb2781ab06351afa0bd8bee236aef2dac6f4c
Yonel Ceruto <yonelceruto@gmail.com>
2017-08-27 15:15:38 -0400

    Fix tests

--- tests/example/tests.py
+++ tests/example/tests.py
@@ -1,4 +1,3 @@
-from __future__ import unicode_literals
 from django.test import TestCase
 from django_ajax.response import JSONResponse
 
@@ -7,6 +6,6 @@ class ResponseTestCase(TestCase):
         data = {'test': True}
         response = JSONResponse(data)
 
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response['Content-Type'], 'application/json')
-        self.assertEqual(response.content, '{"test": true}')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response['Content-Type'], 'application/json')
+        self.assertEquals(response.content, '{"test": true}')
