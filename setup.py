7c7c34f41bd91a103af807c527c463a50418e272
Yonel Ceruto <yonelceruto@gmail.com>
2017-08-27 15:39:44 -0400

    Update programming language

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ CLASSIFIERS = [
     'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.5',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
 ]
